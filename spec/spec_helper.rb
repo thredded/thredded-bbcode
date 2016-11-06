3bce623994c5b1399fb910d38337fdce8f2a7966
Gleb Mazovetskiy <glex.spb@gmail.com>
2016-11-06 15:43:03 -0800

    SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
+if ENV['COVERAGE'] && !%w(rbx jruby).include?(RUBY_ENGINE)
+  require 'simplecov'
+  SimpleCov.command_name 'RSpec'
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'thredded/bbcode'
 
